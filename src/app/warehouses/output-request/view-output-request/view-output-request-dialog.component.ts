8ae1e0879b4afe555ab06f2468a4968d554dffbc
Mohamad Tonbary <ttmohamad299@gmail.com>
2024-01-16 13:55:26 +0200

    output request view

--- src/app/warehouses/output-request/view-output-request/view-output-request-dialog.component.ts
+++ src/app/warehouses/output-request/view-output-request/view-output-request-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MaterialDto, MaterialServiceProxy, WarehouseMaterialDto } from '@shared/service-proxies/service-proxies';
+import { MaterialCodeForDropdownDto, MaterialDto, MaterialServiceProxy, MaterialincludeWarehouseMaterialDto, WarehouseMaterialServiceProxy, WarehouseMaterialWithWarehouseNameAndExpiryDateDto } from '@shared/service-proxies/service-proxies';
 import { OutputRequestDto, OutputRequestServiceProxy, UnitDto, UnitServiceProxy, WarehouseDto, WarehouseServiceProxy } from '@shared/service-proxies/service-proxies';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 
@@ -19,47 +19,105 @@ export class ViewOutputRequestDialogComponent implements OnInit {
   unitNames: UnitDto[] = [];
   warehouseNames: WarehouseDto[] = [];
   warehouseName: WarehouseDto = new WarehouseDto();
+  allMaterialsCodes: MaterialCodeForDropdownDto[] = [];
+  materialsCodes: MaterialCodeForDropdownDto[] = [];
+  allWarehouseMaterials: WarehouseMaterialWithWarehouseNameAndExpiryDateDto[] = [];
+  warehouseMaterials: WarehouseMaterialWithWarehouseNameAndExpiryDateDto[] = [];
+  productIds: number[] = [];
+  materialIds: number[] = [];
+  materialsWithWarehouseMaterials: MaterialincludeWarehouseMaterialDto[] = [];
+
+
   constructor(public bsModalRef: BsModalRef,
     private _outputService: OutputRequestServiceProxy,
     private _unitService: UnitServiceProxy,
     private _warehouseService: WarehouseServiceProxy,
+    private _warehouseMaterialService: WarehouseMaterialServiceProxy,
     private _materialService: MaterialServiceProxy) { }
+
   ngOnInit(): void {
-    this.initOutputRequest()
+    this.initialAllWarehouseMaterial();
+    this.initOutputRequest();
   }
 
   initOutputRequest() {
     this._outputService.get(this.id).subscribe((response: OutputRequestDto) => {
       this.data = response;
       this.loaded = true;
+
       this.data.outputRequestMaterials.forEach((element) => {
+        this.materialIds.push(element.warehouseMaterialId);
         this.initUnitNames(element.unitId);
         this.initWarehouseCode(element.warehouseMaterial.warehouseId);
         this.initMaterialName(element.warehouseMaterial.materialId);
-      })
+      });
 
-    })
+      for (let i = 0; i < this.data.outputRequestProducts.length; i++) {
+        if (i < this.data.outputRequestProducts.length - 1)
+          this.productIds.push(this.data?.outputRequestProducts[i]?.product?.id);
+        else if (i = this.data.outputRequestProducts.length - 1) {
+          this.productIds.push(this.data?.outputRequestProducts[i]?.product?.id);
+          this.initialAllMaterialsCodes(this.productIds);
+        }
+      }      
+    });
   }
+
   initUnitNames(id: number) {
     this._unitService.get(id).subscribe((result) => {
       this.unitName = result;
       this.unitNames.push(this.unitName);
-    })
+    });
   }
+
   initWarehouseCode(id: number) {
     this._warehouseService.get(id).subscribe((result) => {
       this.warehouseName = result;
       this.warehouseNames.push(this.warehouseName);
-    })
-  }
-  initMaterialName(id:number)
-  {
-        this._materialService.get(id).subscribe((result)=>{
-
-          this.material=result;
-        })
+    });
   }
 
+  initMaterialName(id: number) {
+    this._materialService.get(id).subscribe((result) => {
+      this.material = result;
+    });
+  }
+
+  initialAllMaterialsCodes(productIds: number[]) {
+    this._materialService.getByProductsIds(productIds)
+      .subscribe((result) => {
+        this.allMaterialsCodes = result;     
+        
+        this._materialService.getIncludeWarehouseMaterials(this.materialIds)
+          .subscribe((result) => {
+            this.materialsWithWarehouseMaterials = result;
+            this.initilaMaterialsCodes();
+          });          
+      });
+  }
+
+  initilaMaterialsCodes() {
+    this.data.outputRequestMaterials.forEach((element) => {
+      const warehouseMaterial = this.allWarehouseMaterials
+      .find((x)=>x.id == element.warehouseMaterialId);
+      this.warehouseMaterials.push(warehouseMaterial);
 
 
+      const materialId = this.materialsWithWarehouseMaterials
+        .find((x) => x.warehouseMaterials.some((y) => y.id == element.warehouseMaterialId));
+      const material = this.allMaterialsCodes.find((z)=>z.id == materialId.id);
+      this.materialsCodes.push(material);
+    });
+  }
+
+  initialAllWarehouseMaterial() {
+    this._warehouseMaterialService.getWithWarehouseNameAndExpiryDate()
+      .subscribe((result) => {
+        this.allWarehouseMaterials = result;
+      });
+  }
 }
+
+
+
+
